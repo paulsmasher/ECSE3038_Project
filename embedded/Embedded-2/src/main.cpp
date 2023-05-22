3f64db4037945703c047e1cb94424dacc5a3ef56
paulsmasher <123200984+paulsmasher@users.noreply.github.com>
2023-05-21 23:24:07 -0500

    Update main.cpp

--- embedded/Embedded-2/src/main.cpp
+++ embedded/Embedded-2/src/main.cpp
@@ -1,143 +1,136 @@
 #include <Arduino.h>
-#include <Wifi.h>
+#include <WiFi.h>
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
-#include "env.h" 
+#include "env.h"
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
-#define endpoint "rg-lab6-api.onrender.com"
+const char* post_endpoint = API_URL_POST;
+const char* get_endpoint = API_URL_GET;
 
-#define fanPin 22
-#define lightPin 23
-#define presencePin 24
+#define ONE_WIRE_BUS 4
+OneWire one_wire(ONE_WIRE_BUS);
+DallasTemperature temp_sensors(&one_wire);	
 
+const int fan_pin = 22;
+const int light_pin = 23;
+const int pir_pin = 15;
+int pir_state;
 
-float getTemp(){
-
-  return random(21.1,33.1);
-}
-
-int getpresence(){
-
-  return random(0,1);
+float random_float(float min, float max)
+{
+    float scale = rand() / (float) RAND_MAX; /* [0, 1.0] */
+    return min + scale * (max - min);       /* [min, max] */
 }
 
 void setup() {
-
+  temp_sensors.begin();
   Serial.begin(9600);
+  pinMode(fan_pin, OUTPUT);
+  pinMode(light_pin, OUTPUT);
+  pinMode(pir_pin, INPUT);
+  pinMode(ONE_WIRE_BUS, INPUT);
 
-	pinMode(fanPin,OUTPUT);
-  pinMode(lightPin,OUTPUT);
-  pinMode(presencePin,OUTPUT);
-
-	// WiFi_SSID and WIFI_PASS should be stored in the env.h
-  WiFi.begin(WIFI_SSID, WIFI_PASS);
-  Serial.println("");
-	// Connect to wifi
+  WiFi.begin(WIFI_USER, WIFI_PASS);
   Serial.println("Connecting");
-  while(WiFi.status() != WL_CONNECTED) {
+  
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  Serial.println("");
-  Serial.print("Connected to WiFi network with IP Address: ");
-  Serial.println(WiFi.localIP());
+  
+  Serial.println("\nThe Bluetooth Device is Ready to Pair");
+  Serial.print("Connected @");
+  Serial.print(WiFi.localIP());
 }
+
 
 void loop() {
-  //Check WiFi connection status
-  if(WiFi.status()== WL_CONNECTED){
-    Serial.println("");
-    Serial.println("");
+  // Read temperature// Send the command to get temperatures
+  temp_sensors.requestTemperatures(); 
+
+  // Print the temperature in Celsius
+  Serial.print("Temperature: ");
+  float temperature = temp_sensors.getTempCByIndex(0);
+  Serial.print(temperature);
+  Serial.print((char)176); // Shows degrees character
+  Serial.print("C  |  "); 
+  pir_state = digitalRead(pir_pin);
+  Serial.print("\n");
+  Serial.print("");
+  Serial.print(pir_state);
+  Serial.println("");
+  
+  // POST Request
+  if (WiFi.status() == WL_CONNECTED) {   
     HTTPClient http;
-  
-    // Establish a connection to the server
-    String url = "https://" + String(endpoint) + "/temperature";
-    http.begin(url);
-    http.addHeader("Content-type", "application/json");
-
-
-
-    // Specify content-type header
-    //http.addHeader("Content-Type", "application/json");
-
-    StaticJsonDocument<1024> docput;
-    String httpRequestData;
-
-    // Serialise JSON object into a string to be sent to the API
-  
-
-    docput["temperature"] = getTemp();
-    docput["presence"] = getpresence();
-  
-
-    // convert JSON document, doc, to string and copies it into httpRequestData
-    serializeJson(docput, httpRequestData);
-
-    // Send HTTP PUT request
-    int httpResponseCode = http.PUT(httpRequestData);
     String http_response;
 
-    // check reuslt of PUT request. negative response code means server wasn't reached
-    if (httpResponseCode>0) {
-      Serial.print("HTTP Response code: ");
-      Serial.println(httpResponseCode);
+    // POST request
+    http.begin(post_endpoint);
+    http.addHeader("Content-Type", "application/json");
 
-      Serial.print("HTTP Response from server: ");
-      http_response = http.getString();
-      Serial.println(http_response);
-    }
-    else {
-      Serial.print("Error code: ");
-      Serial.println(httpResponseCode);
-    }
+    StaticJsonDocument<1024> post_doc; // Empty JSONDocument
+    String http_request_data; // Emtpy string to be used to store HTTP request data string
+    
+    post_doc["temperature"] = temperature;
+    post_doc["presence"] = !pir_state;
+    serializeJson(post_doc, http_request_data);
 
+    int post_response_code = http.POST(http_request_data);
+
+    if (post_response_code > 0) {
+        Serial.print("Response:");
+        Serial.print(post_response_code);
+    } else {
+        Serial.print("Error: ");
+        Serial.println(post_response_code);
+    }
+      
     http.end();
+      
+   // GET request
+    http.begin(get_endpoint);
+    int http_response_code = http.GET();
 
-
-    url = "https://" + String(endpoint) + "/state";    
-    http.begin(url);
-    httpResponseCode = http.GET();
-
-    Serial.println("");
-    Serial.println("");
-
-    if (httpResponseCode>0) {
-        Serial.print("HTTP Response code: ");
-        Serial.println(httpResponseCode);
-
-        Serial.print("Response from server: ");
+    if (http_response_code > 0) {
+        Serial.print("Response:");
+        Serial.print(http_response_code);
         http_response = http.getString();
         Serial.println(http_response);
-      }
-      else {
-        Serial.print("Error code: ");
-        Serial.println(httpResponseCode);
+    } else {
+        Serial.print("Error: ");
+        Serial.println(http_response_code);
     }
- 
-    StaticJsonDocument<1024> docget;
-
-    DeserializationError error = deserializeJson(docget, http_response);
-
-    if (error) {
-      Serial.print("deserializeJson() failed: ");
-      Serial.println(error.c_str());
-      return;
-    }
-    
-    bool temp = docget["fan"]; 
-    bool light= docget["light"]; 
-    bool presence= docget["presenbce"]; 
-
-    digitalWrite(fanPin,temp);
-    digitalWrite(lightPin,light);
-    digitalWrite(presencePin,presence);
-    
-    // Free resources
+      
     http.end();
+
+    StaticJsonDocument<1024> doc;
+    DeserializationError error = deserializeJson(doc, http_response);
+
+    if (error) { 
+        Serial.print("deserializeJson() failed:");
+        Serial.println(error.c_str());
+        return;
+    }
+      
+    bool light_state = doc["light"];
+    bool fan_state = doc["fan"];
+  
+    Serial.println("Light:");
+    Serial.println(light_state);
+    Serial.println("Fan:");
+    Serial.println(fan_state);
+
+    digitalWrite(fan_pin, fan_state);
+    digitalWrite(light_pin, light_state);
+      
+    Serial.println("Light and Fan Switched Successfully");
+      
+    delay(1000);   
+  } else {
+    Serial.println("Not Connected");
   }
-  else {
-    Serial.println("WiFi Disconnected");
-  }
+  
 }
-
-
